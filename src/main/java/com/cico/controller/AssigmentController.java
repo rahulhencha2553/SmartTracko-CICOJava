c62e5488e5c7acc1e031e0f2d10e98cf965d1094
rahulh.dollop <rahulh.dollop@gmail.com>
2023-08-31 09:50:34 +0530

    rahul hencha

--- src/main/java/com/cico/controller/AssigmentController.java
+++ src/main/java/com/cico/controller/AssigmentController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,9 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.cico.model.Assignment;
-import com.cico.payload.AssignmentQuestionRequest;
 import com.cico.payload.AssignmentRequest;
+import com.cico.payload.AssignmentSubmissionRequest;
 import com.cico.service.IAssignmentService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
@@ -44,28 +47,57 @@ public class AssigmentController {
 
 	@PostMapping("/addQuestionInAssignment")
 	public ResponseEntity<?> addQuestionInAssignment(@RequestParam Map<String, List<MultipartFile>> questionImages) {
-		
-	//	System.out.println(assignmentQuestionData);
-		 for (Map.Entry<String, List<MultipartFile>> entry : questionImages.entrySet()) {
-		        String questionIndex = entry.getKey();
-		        List<MultipartFile> imageList = entry.getValue();
 
-		        // Process imageList based on question index
-		        for (int imageIndex = 0; imageIndex < imageList.size(); imageIndex++) {
-		            MultipartFile imageFile = imageList.get(imageIndex);
-		            // Process the image file (e.g., save to storage, etc.)
-		        }
-		    }
+		// System.out.println(assignmentQuestionData);
+		for (Map.Entry<String, List<MultipartFile>> entry : questionImages.entrySet()) {
+			String questionIndex = entry.getKey();
+			List<MultipartFile> imageList = entry.getValue();
+
+			// Process imageList based on question index
+			for (int imageIndex = 0; imageIndex < imageList.size(); imageIndex++) {
+				MultipartFile imageFile = imageList.get(imageIndex);
+				// Process the image file (e.g., save to storage, etc.)
+			}
+		}
 		return null;
-  }
-	
+	}
+
 	@GetMapping("/getAllAssignments")
-	public ResponseEntity<?> getAllAssignments(){
+	public ResponseEntity<?> getAllAssignments() {
 		return service.getAllAssignments();
 	}
-	
+
 	@GetMapping("/getAssignmentQuesById")
-	public ResponseEntity<?> getAssignmentQuestion(@RequestParam("questionId") Long questionId){
-		return service.getAssignmentQuesById(questionId);
+	public ResponseEntity<?> getAssignmentQuestion(@RequestParam("questionId") Long questionId,
+			@RequestParam("assignmentId") Long assignmentId) {
+		return service.getAssignmentQuesById(questionId, assignmentId);
 	}
+
+	@PostMapping("/submitAssignment")
+	public ResponseEntity<?> submitAssignmentByStudent(@RequestParam("file") MultipartFile file,
+			@RequestParam("assignmentSubmissionRequest") String assignmentSubmissionRequest)
+			throws JsonMappingException, JsonProcessingException {
+		AssignmentSubmissionRequest readValue = objectMapper.readValue(assignmentSubmissionRequest,
+				AssignmentSubmissionRequest.class);
+		return service.submitAssignment(file, readValue);
+	}
+
+	// This API for student Uses
+	@GetMapping("/getSubmitedAssignmetByStudentId")
+	public ResponseEntity<?> getSubmitedAssignmetByStudentId(@RequestParam("studentId") Integer studentId) {
+		return service.getSubmitedAssignmetByStudentId(studentId);
+	}
+
+	// This API for Admin Uses
+	@GetMapping("/getAllSubmitedAssginments")
+	public ResponseEntity<?> getAllSubmitedAssginments() {
+		return service.getAllSubmitedAssginments();
+	}
+
+	@PutMapping("/updateSubmitedAssignmentStatus")
+	public ResponseEntity<?> updateSubmitedAssignmentStatus(@RequestParam("submissionId") Long submissionId,
+			@RequestParam("status") String status,@RequestParam("review") String review) {
+		return service.updateSubmitedAssignmentStatus(submissionId, status, review);
+	}
+
 }
