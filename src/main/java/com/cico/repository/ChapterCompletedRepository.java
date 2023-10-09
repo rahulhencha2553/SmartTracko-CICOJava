2952ff2695576dc4f7cd475da11e3dc4c2057074
rahulh.dollop <rahulh.dollop@gmail.com>
2023-10-09 09:58:01 +0530

    rahul hencha

--- src/main/java/com/cico/repository/ChapterCompletedRepository.java
+++ src/main/java/com/cico/repository/ChapterCompletedRepository.java
@@ -1,14 +1,10 @@
 package com.cico.repository;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.security.access.method.P;
 import org.springframework.stereotype.Repository;
 
-import com.cico.model.Chapter;
 import com.cico.model.ChapterCompleted;
-import com.cico.model.Student;
 
 @Repository
 public interface ChapterCompletedRepository extends JpaRepository<ChapterCompleted, Integer> {
