94a743ca45609c0f2efdc44220f71d4fe0565d02
kapilrathore357 <rathorekapil537@gmail.com>
2023-10-10 16:44:26 +0530

    javakapil

--- src/main/java/com/cico/repository/SubjectRepository.java
+++ src/main/java/com/cico/repository/SubjectRepository.java
@@ -11,13 +11,13 @@ import com.cico.model.Subject;
 
 public interface SubjectRepository extends JpaRepository<Subject, Integer> {
 
-//	@Query("SELECT s FROM Subject s LEFT JOIN FETCH s.chapters  as c WHERE s.subjectName = :subjectName AND s.isDeleted = 0 AND  c.isDeleted = 0")
+//	@Query("SELECT s FROM Subject s LEFT JOIN FETCH s.chapters  as c WHERE s.subjectName = :subjectName AND s.isDeleted = 0 AND  ( c.isDeleted IS NULL OR c.isDeleted = 0)")
 //	Subject findBySubjectNameAndIsDeleted(@Param("subjectName") String subjectName);
 //
-//	@Query("SELECT s FROM Subject s LEFT JOIN FETCH s.chapters as  c WHERE s.subjectId = :subjectId AND  c.isDeleted = 0  ")
+//	@Query("SELECT s FROM Subject s LEFT JOIN FETCH s.chapters as  c WHERE s.subjectId = :subjectId AND  ( c.isDeleted IS NULL OR c.isDeleted = 0) ")
 //	Optional<Subject> findBySubjectIdAndIsDeleted(@Param("subjectId") Integer subjectId);
 //
-//	@Query("SELECT  s FROM Subject s LEFT JOIN FETCH s.chapters as c WHERE  s.isDeleted = 0 AND  c.isDeleted = 0  ")
+//	@Query("SELECT  s FROM Subject s LEFT JOIN FETCH s.chapters as c WHERE  s.isDeleted = 0 AND ( c.isDeleted IS NULL OR c.isDeleted = 0) ")
 //	List<Subject> findByIsDeletedFalse();
 	
 	@Query("SELECT s FROM Subject s  WHERE s.subjectName = :subjectName AND s.isDeleted = 0 ")
