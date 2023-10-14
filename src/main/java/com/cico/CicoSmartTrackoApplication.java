e28d502475c73ac5b9cc8a9cf73fd76475d7845a
rahulh.dollop <rahulh.dollop@gmail.com>
2023-10-14 18:13:12 +0530

    rahul hencha

--- src/main/java/com/cico/CicoSmartTrackoApplication.java
+++ src/main/java/com/cico/CicoSmartTrackoApplication.java
@@ -2,8 +2,10 @@ package com.cico;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CicoSmartTrackoApplication {
 
 	public static void main(String[] args) {
